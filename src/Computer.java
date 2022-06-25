48237ec9c48d7fc3fa200a0a18d687ba57704562
BrayanGuerreroXD <bg7047653@gmail.com>
2022-06-25 10:39:26 -0500

    Se documento la clase Computer implementado javadco

--- src/Computer.java
+++ src/Computer.java
@@ -9,6 +9,13 @@ public class Computer {
     public String name;
     public String type;
 
+    /**
+     * Representa una instancia de la clase Computer.
+     * @param ip número ip del computador.
+     * @param serial número de serial del computador.
+     * @param name nombre del computador.
+     * @param type tipo de computador.
+     */
     public Computer(String ip, int serial, String name, String type) {
         this.ip = ip;
         this.serial = serial;
@@ -16,26 +23,50 @@ public class Computer {
         this.type = type;
     }
 
+    /**
+     * Función que retorna el número ip del computador.
+     * @return número ip
+     */
     private String getIp() {
         return ip;
     }
 
+    /**
+     * Función que retorna el número de serial del computador.
+     * @return número de serial.
+     */
     protected int getSerial() {
         return serial;
     }
 
+    /**
+     * Función que retorna el nombre del computador.
+     * @return nombre.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Función que almacena el nombre del computador.
+     * @param name nombre.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Función que retorna el tipo de computador.
+     * @return tipo de computador.
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Función que retorna un toString de los atributos de la clase.
+     * @return toString.
+     */
     @Override
     public String toString() {
         return "Computer{" +
