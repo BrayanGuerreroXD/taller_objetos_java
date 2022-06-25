6fe190ed9f183c11452c158331b103750c970949
BrayanGuerreroXD <bg7047653@gmail.com>
2022-06-25 10:24:55 -0500

    Se documento la clase BankAccount donde se implemento javadoc

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -4,18 +4,37 @@
  * @author Brayan Stewart Guerrero Ordoñez
  */
 public class BankAccount {
+    /**
+     * Representa la variable que almacena el numero de cuenta.
+     */
     private int accountNumber;
+    /**
+     * Representa la variable que determina el estado de la cuenta bancaria.
+     */
     boolean activate;
 
+    /**
+     * Representa una instancia de la clase BankAccount.
+     * @param accountNumber numero de cuenta.
+     * @param activate estado de la cuenta.
+     */
     public BankAccount(int accountNumber, boolean activate) {
         this.accountNumber = accountNumber;
         this.activate = activate;
     }
 
+    /**
+     * Función que returna el estado de la cuenta bancaria (True = Activo , False = No Activo).
+     * @return estado de cuenta.
+     */
     public boolean isActivate() {
         return activate;
     }
 
+    /**
+     * Función que almacena el estado de la cuenta bancaria.
+     * @param activate estado de cuenta.
+     */
     public void setActivate(boolean activate) {
         this.activate = activate;
     }
